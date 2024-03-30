3a6dd1d357d48d3ed5737c55c95ef9ba206e6983
quisimbryant <quisim_bryant@outlook.com>
2024-03-30 16:50:41 -0400

    New changes

--- swift/hello.swift
+++ swift/hello.swift
@@ -2,3 +2,18 @@ print("Hello, world!")
 print("Goodbye, world!")
 print("😭")
 
+print("       1")
+print("     2 3")
+print("   4 5 6")
+print("7 8 9 10")
+
+// Using multiple lines for this.
+// Best not be too exhaustive
+
+print("Dear Self,")
+print("I hope that you've had fun learning Swift.")
+print("Now, you may be able to port macOS apps to Linux.")
+print("I'm glad you're always searching for new knowledge.")
+print("Now, let's see what the next adventure is!")
+print("Quisim, 03/30/2024")
+
